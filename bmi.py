87ad5b406fc44d0968d00005382a9716cab3ec66
Charissa Chua <48579397+charissachua@users.noreply.github.com>
2020-01-14 11:42:57 +0800

    source codes for CET Python Day 1

---
+++ bmi.py
@@ -0,0 +1,17 @@
+weight = input("Enter your weight (kg):")
+height = input("Enter your height (m):")
+
+weight = float(weight)
+height = float(height)
+
+bmi = weight/(height * height)
+
+print("bmi: " + str(bmi))
+
+if bmi < 18:
+    print ("You are underweight.")
+elif bmi <25:
+elif bmi < 30:
+    print("You are overweight.")
+else:
+    print("You are obese.")
